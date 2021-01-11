f448e8bf6c88b36c44d2c062363ec8d4f27493dc
ivyoverflow <belikaualiaksandr@gmail.com>
2021-01-11 11:27:23 +0300

    Fix client-side publisher handler

--- client/internal/handler/publisher.go
+++ client/internal/handler/publisher.go
@@ -21,6 +21,8 @@ func NewPublisherHandler(logger *logger.Logger) *PublisherHandler {
 
 // Publish connects to the server and sends a request.
 func (handler *PublisherHandler) Publish(ws *websocket.Conn) {
+	// connect to our server.
+	serverWs, err := websocket.Dial("ws://localhost:8080/publish", "", "http://localhost:8080")
 	for {
 		request := &model.PublishRequest{}
 		if err := websocket.JSON.Receive(ws, request); err != nil {
@@ -30,15 +32,12 @@ func (handler *PublisherHandler) Publish(ws *websocket.Conn) {
 			return
 		}
 
-		// connect to our server.
-		serverWs, err := websocket.Dial("ws://localhost:8080/publish", "", "http://localhost:8080")
 		if err != nil {
 			handler.logger.Error(err.Error())
 			fmt.Fprintf(ws, fmt.Sprintf(`{"error": {"statusCode: %d", "message: %s"}}`, http.StatusInternalServerError, err.Error()))
 
 			return
 		}
-
 		// sending data to the server.
 		if err = websocket.JSON.Send(serverWs, request); err != nil {
 			handler.logger.Error(err.Error())
@@ -46,7 +45,6 @@ func (handler *PublisherHandler) Publish(ws *websocket.Conn) {
 
 			return
 		}
-
 		handler.logger.Debug("The publisher sends a new message request to the server")
 	}
 }
